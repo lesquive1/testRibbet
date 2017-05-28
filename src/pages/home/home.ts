7457fe577ca0c7cd0cad9aa78436402fbf600dc2
Luis Esquivel <laesquivel@hotmail.com>
2017-05-27 22:49:56 -0400

    fetching reward points

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  posts: any;
+
+  constructor(public navCtrl: NavController,
+              public http: Http) {
+
+    this.http.get('/api').map(res => res.json()).subscribe(data => {
+        this.posts = data.reward_points;
+        console.log(this.posts);
+    });
 
   }
 
